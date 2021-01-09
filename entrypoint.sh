7bcc4d9e1808d1a8281d21c0f91871e6d37b53a4
fbjorn <kopfabschneider@yandex.ru>
2021-01-09 23:44:07 +0300

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-echo "${1}" | base64 -D > /tmp/template.tpl
+echo "${1}" | base64 -d > /tmp/template.tpl
 result=$(parse-template /tmp/template.tpl)
 
 echo "::set-output name=time::$result"
